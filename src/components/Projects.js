7f3215618e3762c01589c6ef0b07b06dba7b7ae5
matthewmckeon <mattmckeon2@gmail.com>
2021-03-14 22:27:15 -0400

    image mapping

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import projectData from '../data';
 import '../styles/Projects.css';
-import RoundsAppLive from '../images/RoundsAppLive.png';
 
 const Projects = () => {
   return (
@@ -11,7 +10,7 @@ const Projects = () => {
         <h1 className='projects-title'>PROJECTS</h1>
         {projectData.map((project) => {
           return (
-            <Row className='project-content'>
+            <Row className='project-content' key={project.alt}>
               <Col lg={4} sm={12}>
                 <h3 className='project-name'>{project.title}</h3>
                 <p>{project.info}</p>
@@ -32,8 +31,8 @@ const Projects = () => {
               </Col>
               <Col lg={8} sm={12}>
                 <img
-                  src={RoundsAppLive}
-                  alt='RoundsApp'
+                  src={project.img}
+                  alt={project.alt}
                   width='700vw'
                   height='auto'
                   className='project-image'
--- src/data.js
+++ src/data.js
@@ -1,19 +1,20 @@
+import RoundsPic from './images/RoundsPic.png'
+import SwansonPic from './images/SwansonPic.png'
+
 const projectData = [
   {
     title: 'Rounds App',
     info: 'Anim cillum do incididunt elit aliquip sint. Mollit tempor aute officia minim nulla. Esse duis sint do eu cupidatat pariatur in nostrud occaecat aliqua. Adipisicing quis dolore ullamco commodo cupidatat veniam adipisicing cupidatat laboris. Dolor aliquip velit dolor et.',
+    img: RoundsPic,
+    alt: 'RoundsApp',
     url: 'http://rounds-golf.heroku.app',
     repo: 'https://github.com/matthewmckeon/rounds',
   },
   {
-    title: 'Rounds App',
+    title: 'Ron Swanson Quote Generator',
     info: 'Anim cillum do incididunt elit aliquip sint. Mollit tempor aute officia minim nulla. Esse duis sint do eu cupidatat pariatur in nostrud occaecat aliqua. Adipisicing quis dolore ullamco commodo cupidatat veniam adipisicing cupidatat laboris. Dolor aliquip velit dolor et.',
-    url: 'http://rounds-golf.heroku.app',
-    repo: 'https://github.com/matthewmckeon/rounds',
-  },
-  {
-    title: 'Rounds App',
-    info: 'Anim cillum do incididunt elit aliquip sint. Mollit tempor aute officia minim nulla. Esse duis sint do eu cupidatat pariatur in nostrud occaecat aliqua. Adipisicing quis dolore ullamco commodo cupidatat veniam adipisicing cupidatat laboris. Dolor aliquip velit dolor et.',
+    img: SwansonPic,
+    alt: 'Ron Swanson',
     url: 'http://rounds-golf.heroku.app',
     repo: 'https://github.com/matthewmckeon/rounds',
   },
